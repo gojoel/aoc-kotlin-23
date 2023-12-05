f49b826109e74ef4f11c520a86482c69f6f009a1
Joel Goncalves <goncalves.joel95@gmail.com>
2023-12-04 20:02:40 -0500

    Day03 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,128 +1,140 @@
-import java.lang.Integer.max
 import java.lang.StringBuilder
 
 fun main() {
 
-    fun buildLeft(line: String, start: Int): Int {
+    fun buildLeft(array: Array<CharArray>, row: Int, start: Int): String {
         val partNumber = StringBuilder()
         var startPos = start
-        while (startPos >= 0 && line[startPos].isDigit()) {
-            partNumber.insert(0, line[startPos])
+        while (startPos >= 0 && array[row][startPos].isDigit()) {
+            partNumber.insert(0, array[row][startPos])
+            array[row][startPos] = '.'
             startPos -= 1
         }
 
-        if (partNumber.isEmpty()) {
-            return -1
-        }
-
-        return partNumber.toString().toInt()
+        return partNumber.toString()
     }
 
-    fun buildRight(line: String, start: Int): Int {
+    fun buildRight(array: Array<CharArray>, row: Int, start: Int): String {
         val partNumber = StringBuilder()
         var startPos = start
-        while (startPos < line.length && line[startPos].isDigit()) {
-            partNumber.append(line[startPos])
+        while (startPos < array[row].size && array[row][startPos].isDigit()) {
+            partNumber.append(array[row][startPos])
+            array[row][startPos] = '.'
             startPos += 1
         }
 
-        if (partNumber.isEmpty()) {
-            return -1
-        }
-
-        return partNumber.toString().toInt()
+        return partNumber.toString()
     }
 
-    fun buildLR(line: String, startPos: Int): Int {
-        if (line[startPos].isDigit()) {
+    fun buildLR(array: Array<CharArray>, row: Int, start: Int): String {
+        if (array[row][start].isDigit()) {
             val partNumber = StringBuilder()
-            partNumber.append(line[startPos])
+            partNumber.append(array[row][start])
+            array[row][start] = '.'
 
-            val left = buildLeft(line, startPos - 1)
-            val right = buildRight(line, startPos + 1)
-            if (left > -1) {
-                partNumber.insert(0, left.toString())
-            }
+            partNumber.insert(0, buildLeft(array, row, start - 1))
+            partNumber.append(buildRight(array, row, start + 1))
 
-            if (right > -1) {
-                partNumber.append(right.toString())
-            }
-
-            return partNumber.toString().toInt()
+            return partNumber.toString()
         }
 
-        return  -1
+        return ""
     }
 
-    fun buildVertical(line: String, startPos: Int): Int {
-        return if (line[startPos].isDigit()) {
-            max(buildLR(line, startPos), 0)
+    fun buildVertical(array: Array<CharArray>, row: Int, start: Int): List<String> {
+        val partNumbers = mutableListOf<String>()
+        if (array[row][start].isDigit()) {
+            partNumbers.add(buildLR(array, row, start))
         } else {
-            var sum = 0
-            if (startPos - 1 >= 0 ) {
-                sum += max(buildLeft(line, startPos - 1), 0)
+            if (start - 1 >= 0) {
+                partNumbers.add(buildLeft(array, row, start - 1))
             }
 
-            if (startPos + 1 < line.length ) {
-                sum += max(buildRight(line, startPos + 1), 0)
+            if (start + 1 < array[row].size) {
+                partNumbers.add(buildRight(array, row, start + 1))
             }
-
-            sum
         }
+
+        return partNumbers
     }
 
+    fun partNumberInt(partNumber: String): Int {
+        if (partNumber.isEmpty()) {
+            return 0
+        }
+
+        return partNumber.toInt()
+    }
+
+    fun getPartNumbers(array: Array<CharArray>, symbol: Pair<Int, Int>): List<Int> {
+        val partNumbers = mutableListOf<Int>()
+        partNumbers.add(partNumberInt(buildLeft(array, symbol.first, symbol.second - 1)))
+        partNumbers.add(partNumberInt(buildRight(array, symbol.first, symbol.second + 1)))
+
+        val topRow = symbol.first - 1
+        if (topRow >= 0) {
+            buildVertical(
+                array,
+                topRow,
+                symbol.second
+            ).forEach { partNumbers.add(partNumberInt(it)) }
+        }
+
+        val bottomRow = symbol.first + 1
+        if (bottomRow < array.size) {
+            buildVertical(array, bottomRow, symbol.second).forEach {
+                partNumbers.add(
+                    partNumberInt(
+                        it
+                    )
+                )
+            }
+        }
+
+        return partNumbers
+    }
 
     fun part1(input: List<String>): Int {
-
+        val array = Array(input.size) { CharArray(input[0].length) }
         val symbols = mutableListOf<Pair<Int, Int>>()
 
         input.forEachIndexed { lineIdx, line ->
             line.forEachIndexed { charIdx, char ->
+                array[lineIdx][charIdx] = char
                 if (!char.isDigit() && char != '.') {
                     symbols.add(Pair(lineIdx, charIdx))
                 }
             }
         }
 
-        var sum = 0
-        symbols.forEach { symbol ->
-            sum += max(buildLeft(input[symbol.first], symbol.second - 1), 0)
-            sum += max(buildRight(input[symbol.first], symbol.second + 1), 0)
+        return symbols.map { getPartNumbers(array, it) }.flatten().sum()
+    }
 
-            val topRow = symbol.first - 1
-            if (topRow >= 0) {
-                sum += buildVertical(input[topRow], symbol.second)
-            }
+    fun part2(input: List<String>): Int {
+        val array = Array(input.size) { CharArray(input[0].length) }
+        val symbols = mutableListOf<Pair<Int, Int>>()
 
-            val bottomRow = symbol.first + 1
-            if (bottomRow < input.size) {
-                sum += buildVertical(input[bottomRow], symbol.second)
+        input.forEachIndexed { lineIdx, line ->
+            line.forEachIndexed { charIdx, char ->
+                array[lineIdx][charIdx] = char
+                if (!char.isDigit() && char != '.') {
+                    symbols.add(Pair(lineIdx, charIdx))
+                }
             }
         }
 
-        return sum
-    }
-
-
-
-    fun part2(input: List<String>): Int {
-        return input.size
+        return symbols.map { getPartNumbers(array, it) }.map { it.filter { num -> num != 0 } }
+            .filter {
+                it.size == 2
+            }.sumOf { it.reduce { acc, i -> acc * i } }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    println(part1(testInput))
     check(part1(testInput) == 4361)
-
-//    val testInput2 = readInput("Day03_test_2")
-//    println(part1(testInput2))
-//    check(part1(testInput2) == 10279)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
-
-// submitted
-// 513986
-// 522014
